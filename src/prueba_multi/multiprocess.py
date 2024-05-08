614f10c2a0dc96e169856f5a0359dab8cdb8be52
j.morales5162 <josemanuelmoralesgonzalez9@gmail.com>
2024-05-08 12:40:48 +0200

    Multiprocess Pathos and Process class

--- src/prueba_multi/multiprocess.py
+++ src/prueba_multi/multiprocess.py
@@ -10,7 +10,7 @@ import joblib
 from graficar import graficarResultados
 from sklearn.ensemble import IsolationForest
 from sklearn.pipeline import make_pipeline
-
+from multiprocessing import Process
 from cross_validation import cross_validation_regression
 from sklearn.cluster import KMeans
 
@@ -33,35 +33,41 @@ def adestrarMetodo(datafile, metodo, nome, depVars, indepVars):
     output_path.parent.mkdir(parents=True, exist_ok=True)
     joblib.dump(model, output_path)
 
-"""
-def multi_process_regression():
+def multi_process_regression_pathos():
     with ProcessingPool() as pool:
         # Añadir las funciones al pool para su ejecución asíncrona
-        resultado1 = pool.apipe(adestrarMetodo, Config.path, Config.mr['lr_pipe'], Config.mr['lr'], Config.depVars, Config.indepVars)
-        resultado2 = pool.apipe(adestrarMetodo, Config.path, Config.mr['poly_pipe'], Config.mr['poly'], Config.depVars, Config.indepVars)
+        #resultado1 = pool.apipe(adestrarMetodo, Config.path, Config.mr['lr_pipe'], Config.mr['lr'], Config.depVars, Config.indepVars)
+        #resultado2 = pool.apipe(adestrarMetodo, Config.path, Config.mr['poly_pipe'], Config.mr['poly'], Config.depVars, Config.indepVars)
         resultado3 = pool.apipe(adestrarMetodo, Config.path, Config.mr['gBoosting_pipe'], Config.mr['gBoosting'], Config.depVars, Config.indepVars)
-        resultado4 = pool.apipe(adestrarMetodo, Config.path, Config.mr['rf_pipe'], Config.mr['rf'], Config.depVars, Config.indepVars)
+        #resultado4 = pool.apipe(adestrarMetodo, Config.path, Config.mr['rf_pipe'], Config.mr['rf'], Config.depVars, Config.indepVars)
         
         # Esperar a que finalicen las funciones y obtener los resultados
-        resultado1.get()
-        resultado2.get()
+        #resultado1.get()
+        #resultado2.get()
         resultado3.get()
-        resultado4.get()
-"""
-        
+        #resultado4.get()
+
+def multi_process_regression_constructor():
+    procesos = []
+    proceso1 = Process(target=adestrarMetodo, args=(Config.path, Config.mr['gBoosting_pipe'], Config.mr['gBoosting'], Config.depVars, Config.indepVars))
+    proceso1.start()
+    procesos.append(proceso1)
+    for proceso in procesos:
+        proceso.join()
+
 def single_process_regression():
-    adestrarMetodo(Config.path, Config.mr['lr_pipe'], Config.mr['lr'], Config.depVars, Config.indepVars)
-    adestrarMetodo(Config.path, Config.mr['poly_pipe'], Config.mr['poly'], Config.depVars, Config.indepVars)
+    #adestrarMetodo(Config.path, Config.mr['lr_pipe'], Config.mr['lr'], Config.depVars, Config.indepVars)
+    #adestrarMetodo(Config.path, Config.mr['poly_pipe'], Config.mr['poly'], Config.depVars, Config.indepVars)
     adestrarMetodo(Config.path, Config.mr['gBoosting_pipe'], Config.mr['gBoosting'], Config.depVars, Config.indepVars)
-    adestrarMetodo(Config.path, Config.mr['rf_pipe'], Config.mr['rf'], Config.depVars, Config.indepVars)
+    #adestrarMetodo(Config.path, Config.mr['rf_pipe'], Config.mr['rf'], Config.depVars, Config.indepVars)
 
 if __name__ == "__main__":
-
-    # Medir el tiempo de ejecución para cada método
-    #time_single = timeit("single_process_regression()", globals=globals(), number=1)
-    #print(f"Tiempo de ejecución secuencial: {time_single/60:.2f} min")
-
-    time_multi = timeit("multi_process_regression()", globals=globals(), number=1)
-    print(f"Tiempo de ejecución con multiprocesos: {time_multi/60:.2f} min")
-
     
+    tiempos = {}
+
+    tiempos['single_process'] = timeit("single_process_regression()", globals=globals(), number=1)
+    tiempos['process_with_pathos'] = timeit("multi_process_regression_pathos()", globals=globals(), number=1)
+    tiempos['process_with_constructor'] = timeit("multi_process_regression_constructor()", globals=globals(), number=1)
+
+    for metodo,tiempo in tiempos.items():
+        print(f"Tiempo de ejecución para {metodo}: {tiempo/60:.2f} min")
