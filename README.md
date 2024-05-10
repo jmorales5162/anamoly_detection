c3f0dd7481b39088b77f8376eadf0c18ba6b3501
j.morales5162 <josemanuelmoralesgonzalez9@gmail.com>
2024-05-10 23:57:24 +0200

    anomalies pathos

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
-# Anamoly detection 
+# Técnicas de regresión y detección de anomalías con Machine Learning
 
 ## Dataset Kaggle:
 https://www.kaggle.com/datasets/anikannal/solar-power-generation-data
 
 
 ## Instruccions para instalar
-    git clone https://github.com/jmorales5162/anamoly_detection.git
-    cd anamoly_detection
+    git clone https://github.com/jmorales5162/anomaly_detection.git
+    cd anomaly_detection
     pip install -r requirements.txt
-    kaggle datasets download -d sujaykapadnis/emotion-recognition-dataset
-    unzip emotion-recognition-dataset.zip; rm emotion-recognition-dataset.zip
+    kaggle datasets download -d anikannal/solar-power-generation-data
+    unzip archive.zip 
     python main.py
+
+
--- src/main.py
+++ src/main.py
@@ -19,51 +19,8 @@ def process_df():
     df_solar = pd.merge(generation_data.drop(columns = ['PLANT_ID']), weather_data.drop(columns = ['PLANT_ID', 'SOURCE_KEY']), on='DATE_TIME')
     return df_solar.drop(columns = ['DATE_TIME', 'SOURCE_KEY', 'DAILY_YIELD', 'TOTAL_YIELD', 'AMBIENT_TEMPERATURE', 'AC_POWER'])
 
-def single_process_regression(rm):
-    """Se crean los procesos de regresion secuenciales de la clase Process
-
-    :param rm: Objeto de la clase RegressionMethod.
-    :type rm: object"""
-    rm.adestrarMetodo(Config.mr['lr_pipe'],Config.mr['lr'])
-    rm.adestrarMetodo(Config.mr['poly_pipe'],Config.mr['poly'])
-    rm.adestrarMetodo(Config.mr['gBoosting_pipe'],Config.mr['gBoosting'])
-    rm.adestrarMetodo(Config.mr['rf_pipe'],Config.mr['rf'])
-
-def single_process_anomalies(am):
-    """Se crean los procesos de detección de anomalías secuenciales de la clase Process
-
-    :param am: Objeto de la clase AnomaliesMethod.
-    :type am: object"""
-    am.isolationForest(Config.contamination)
-    am.kmeans(Config.n_clusters)
-    am.autoEncoder()
-
-def multi_process_constructor(procesos):
-    """Ejecuta una lista de procesos
-
-    :param procesos: Lista de procesos a ejecutar
-    :type procesos: list"""
-    for proceso in procesos:
-        proceso.start()    
-    for proceso in procesos:
-        proceso.join()
-
-def multi_process_pathos():
-    """Ejecuta una lista de procesos
-
-    :param procesos: Lista de procesos a ejecutar
-    :type procesos: list"""
-    with ProcessingPool() as pool:
-        rm_1 = pool.apipe(rm.adestrarMetodo, Config.mr['lr_pipe'], Config.mr['lr'])
-        rm_2 = pool.apipe(rm.adestrarMetodo, Config.mr['poly_pipe'], Config.mr['poly'])
-        rm_3 = pool.apipe(rm.adestrarMetodo, Config.mr['gBoosting_pipe'], Config.mr['gBoosting'])
-        rm_4 = pool.apipe(rm.adestrarMetodo, Config.mr['rf_pipe'], Config.mr['rf'])
-    procesos = [rm_1,rm_2,rm_3,rm_4] 
-    for proceso in procesos:
-        proceso.get()
-
 def create_process():
-    """Crea las listas de procesos para los modelos de regresión"""
+    """Crea las listas de procesos para los modelos de regresión y de detección de anomalías"""
     rm1 = Process(target= rm.adestrarMetodo, args=(Config.mr['lr_pipe'], Config.mr['lr']))
     rm2 = Process(target= rm.adestrarMetodo, args=(Config.mr['poly_pipe'], Config.mr['poly']))
     rm3 = Process(target= rm.adestrarMetodo, args=(Config.mr['gBoosting_pipe'], Config.mr['gBoosting']))
@@ -77,6 +34,61 @@ def create_process():
   
     return process_list, anomalies_process 
 
+def single_process_anomalies(am):
+    """Se crean los procesos de detección de anomalías secuenciales de la clase Process
+
+    :param am: Objeto de la clase AnomaliesMethod.
+    :type am: object"""
+    am.isolationForest(Config.contamination)
+    am.kmeans(Config.n_clusters)
+    am.autoEncoder()
+
+def single_process_regression(rm):
+    """Se crean los procesos de regresion secuenciales de la clase Process
+
+    :param rm: Objeto de la clase RegressionMethod.
+    :type rm: object"""
+    rm.adestrarMetodo(Config.mr['lr_pipe'],Config.mr['lr'])
+    rm.adestrarMetodo(Config.mr['poly_pipe'],Config.mr['poly'])
+    rm.adestrarMetodo(Config.mr['gBoosting_pipe'],Config.mr['gBoosting'])
+    rm.adestrarMetodo(Config.mr['rf_pipe'],Config.mr['rf'])
+
+def multi_process_constructor(procesos):
+    """Ejecuta una lista de procesos
+
+    :param procesos: Lista de procesos a ejecutar
+    :type procesos: list"""
+    for proceso in procesos:
+        proceso.start()    
+    for proceso in procesos:
+        proceso.join()
+
+def anomalies_process_with_pathos():
+    """Ejecuta una lista de procesos de detección de anomalías
+
+    :param procesos: Lista de procesos a ejecutar
+    :type procesos: list"""
+    with ProcessingPool() as pool:
+        rm_1 = pool.apipe(am.isolationForest, Config.contamination)
+        rm_2 = pool.apipe(am.kmeans, Config.n_clusters)
+        rm_3 = pool.apipe(am.autoEncoder)
+    procesos = [rm_1,rm_2,rm_3] 
+    for proceso in procesos:
+        proceso.get()
+
+def regression_process_with_pathos():
+    """Ejecuta una lista de procesos de regresion
+
+    :param procesos: Lista de procesos a ejecutar
+    :type procesos: list"""
+    with ProcessingPool() as pool:
+        rm_1 = pool.apipe(rm.adestrarMetodo, Config.mr['lr_pipe'], Config.mr['lr'])
+        rm_2 = pool.apipe(rm.adestrarMetodo, Config.mr['poly_pipe'], Config.mr['poly'])
+        rm_3 = pool.apipe(rm.adestrarMetodo, Config.mr['gBoosting_pipe'], Config.mr['gBoosting'])
+        rm_4 = pool.apipe(rm.adestrarMetodo, Config.mr['rf_pipe'], Config.mr['rf'])
+    procesos = [rm_1,rm_2,rm_3,rm_4] 
+    for proceso in procesos:
+        proceso.get()
 
 if __name__ == "__main__":
     
@@ -89,13 +101,12 @@ if __name__ == "__main__":
 
     tiempos['single_process_anomalies'] = timeit("single_process_anomalies(am)", globals=globals(), number=1)
     tiempos['anomalies_process_with_constructor'] = timeit("multi_process_constructor(anomalies_process)", globals=globals(), number=1)
+    tiempos['anomalies_process_with_pathos'] = timeit("anomalies_process_with_pathos()", globals=globals(), number=1) 
 
     tiempos['single_process_regression'] = timeit("single_process_regression(rm)", globals=globals(), number=1)
     tiempos['regression_process_with_constructor'] = timeit("multi_process_constructor(process_list)", globals=globals(), number=1)
+    tiempos['regression_process_with_pathos'] = timeit("regression_process_with_pathos()", globals=globals(), number=1)
     
-    tiempos['regression_process_with_pathos'] = timeit("multi_process_pathos()", globals=globals(), number=1)
-    
-
     datos = pd.DataFrame(tiempos, index=[0])
     datos.to_excel("./src/tiempos.xlsx", index=False)
 
